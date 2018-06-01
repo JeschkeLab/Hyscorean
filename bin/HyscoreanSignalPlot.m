050ecad259eb57876a63d99bceb7d05177df65f4
Luis Fabregas <luisf@student.ethz.ch>
2018-06-01 08:57:23 +0200

    Bugs on detached signal plots fixed

--- bin/HyscoreanSignalPlot.m
+++ bin/HyscoreanSignalPlot.m
@@ -6,9 +6,9 @@ cla(handles.signal_t1)
     handles.PlotProcessedSignal = true;
  end
   if ~isfield(handles,'PlotBackground')     
-   PlotBackground = false;
+    PlotSecondCorrection = false;
   else
-    PlotBackground = get(handles.PlotBackground,'Value');
+    PlotSecondCorrection = get(handles.PlotBackground,'Value');
   end
   
   ylimMax = max(max(real(Processed.Signal)));
@@ -88,7 +88,7 @@ if ~get(handles.ChangeSignalPlotDimension,'Value')
       hold(handles.signal_t1,'on')
   end
   
-    if PlotBackground
+    if PlotSecondCorrection
        trace1 = real(handles.Data.FirstBackgroundCorrected(:,position1));
     trace1 = trace1 - mean(real(handles.Data.FirstBackgroundCorrected(end,:)));
     trace1 = trace1/max(max(real(handles.Data.FirstBackgroundCorrected)));
@@ -102,11 +102,10 @@ if ~get(handles.ChangeSignalPlotDimension,'Value')
       Axis = linspace(min(handles.Data.CorrectedTimeAxis1),max(handles.Data.CorrectedTimeAxis1),length(trace1));      
       plot(handles.signal_t1,Axis,trace1,'Color',[0.6 0.0 0.8])     
       hold(handles.signal_t1,'on')
-      P1 = Axis(handles.Data.BackgroundStartIndex1)*[1 1];
+      P1 = Axis(handles.Data.BackgroundStartIndex2)*[1 1];
       P2 = [1.1*ylimMin 1.1*ylimMax];
       line(handles.signal_t1,P1,P2,'Color',[0.6 0.0 0.8],'LineStyle','--')
-      text(1.1*Axis(handles.Data.BackgroundStartIndex1),ylimMax,sprintf('%i ns',round(1000*Axis(handles.Data.BackgroundStartIndex1),0)),'Color',[0.6 0.0 0.8])
-
+      text(1.1*Axis(handles.Data.BackgroundStartIndex2),ylimMax,sprintf('%i ns',round(1000*Axis(handles.Data.BackgroundStartIndex2),0)),'Color',[0.6 0.0 0.8])
     end
 hold(handles.signal_t1,'off')
 % set(handles.signal_t1,'ytick',[])
@@ -119,6 +118,7 @@ set(handles.trace2Info,'string',sprintf('Trace along t1 at t2 = %g ns',round(100
 set(handles.signal_t1,'FontSize',8)
 
 else
+  
   position1 = round(get(handles.t1_Slider,'Value'));
   TimeAxis2 = Processed.TimeAxis1(1:length(Processed.TimeAxis1)-str2double(get(handles.ZeroFilling1,'String')));
   
@@ -132,13 +132,23 @@ else
 
   end
   if get(handles.NonCorrectedTrace,'value')
-    trace2 = real(handles.Data.NonCorrectedIntegral(position1,:));
-    trace2 = trace2 - mean(real(handles.Data.NonCorrectedIntegral(:,end)));
-    trace2 = trace2/max(max(real(handles.Data.NonCorrectedIntegral)));
-    Axis = linspace(min(handles.Data.CorrectedTimeAxis2),max(handles.Data.CorrectedTimeAxis2),length(trace2));
-    plot(handles.signal_t1,Axis,trace2,'Color',[0.2 0.2 1])
+  trace1 = real(handles.Data.NonCorrectedIntegral(position1,:));
+    trace1 = trace1 - mean(real(handles.Data.NonCorrectedIntegral(end,:)));
+    trace1 = trace1/max(max(real(handles.Data.NonCorrectedIntegral)));
+%     trace1 = trace1/max(abs(trace1));
+    Axis = linspace(min(handles.Data.CorrectedTimeAxis1),max(handles.Data.CorrectedTimeAxis1),length(trace1));
+    plot(handles.signal_t1,Axis,trace1,'Color',[0.2 0.2 0.9])
       hold(handles.signal_t1,'on')
-
+     trace1 = real(handles.Data.Background1(:,position1));
+      trace1 = trace1 - mean(real(handles.Data.Background1(end,:)));
+      trace1 = trace1/max(max(real(handles.Data.NonCorrectedIntegral)));
+      Axis = linspace(min(handles.Data.CorrectedTimeAxis1),max(handles.Data.CorrectedTimeAxis1),length(trace1));      
+      plot(handles.signal_t1,Axis,trace1,'Color',[0.2 0.2 0.9])     
+      hold(handles.signal_t1,'on')
+      P1 = Axis(handles.Data.BackgroundStartIndex1)*[1 1];
+      P2 = [1.1*ylimMin 1.1*ylimMax];
+      line(handles.signal_t1,P1,P2,'Color',[0.2 0.2 0.9],'LineStyle','--')
+      text(1.1*Axis(handles.Data.BackgroundStartIndex1),ylimMax,sprintf('%i ns',round(1000*Axis(handles.Data.BackgroundStartIndex1),0)),'Color',[0.2 0.2 0.9])
   end
   if get(handles.PreProcessedTrace,'value')
     trace2 = real(handles.Data.PreProcessedSignal(position1,:));
@@ -169,18 +179,24 @@ else
       hold(handles.signal_t1,'on')
 
   end
-  if PlotBackground
-      trace1 = real(handles.Data.Background1(:,position1));
-      trace1 = trace1 - mean(real(handles.Data.Background1(end,:)));
-      trace1 = trace1/max(max(real(handles.Data.NonCorrectedIntegral)));
+  if PlotSecondCorrection
+      trace1 = real(handles.Data.FirstBackgroundCorrected(position1,:));
+      trace1 = trace1 - mean(real(handles.Data.FirstBackgroundCorrected(end,:)));
+      trace1 = trace1/max(max(real(handles.Data.FirstBackgroundCorrected)));
+      %     trace1 = trace1/max(abs(trace1));
+      Axis = linspace(min(handles.Data.CorrectedTimeAxis1),max(handles.Data.CorrectedTimeAxis1),length(trace1));
+      plot(handles.signal_t1,Axis,trace1,'Color',[0.6 0.0 0.8])
+      hold(handles.signal_t1,'on')
+      trace1 = real(handles.Data.Background2(position1,:));
+      trace1 = trace1 - mean(real(handles.Data.Background2(end,:)));
+      trace1 = trace1/max(max(real(handles.Data.FirstBackgroundCorrected)));
       Axis = linspace(min(handles.Data.CorrectedTimeAxis1),max(handles.Data.CorrectedTimeAxis1),length(trace1));      
       plot(handles.signal_t1,Axis,trace1,'Color',[0.6 0.0 0.8])     
       hold(handles.signal_t1,'on')
-      P1 = Axis(handles.Data.BackgroundStartIndex1)*[1 1];
+      P1 = Axis(handles.Data.BackgroundStartIndex2)*[1 1];
       P2 = [1.1*ylimMin 1.1*ylimMax];
       line(handles.signal_t1,P1,P2,'Color',[0.6 0.0 0.8],'LineStyle','--')
-      text(1.1*Axis(handles.Data.BackgroundStartIndex1),ylimMax,sprintf('%i ns',round(1000*Axis(handles.Data.BackgroundStartIndex1),0)),'Color',[0.6 0.0 0.8])
-
+      text(1.1*Axis(handles.Data.BackgroundStartIndex2),ylimMax,sprintf('%i ns',round(1000*Axis(handles.Data.BackgroundStartIndex2),0)),'Color',[0.6 0.0 0.8])
     end
 hold(handles.signal_t1,'off')
 
