eda5bc0f58d1bf6c4ff6d6d262b2b11b987b6c26
Peter Keresztes Schmidt <peterke@student.ethz.ch>
2019-02-19 14:26:55 +0100

    Optimize camera_functional for speed
    By precalulating abs(X) it is possible to shave of 50% of runtime (1.3s -> 0.6s in my testcase)

--- bin/camera_functional.m
+++ bin/camera_functional.m
@@ -2,11 +2,14 @@
 % of the hoch-hore entropy functional.
 function [f, df] = camera_functional (X, d)
   % compute the scalar sum.
-  f = sum(sum(abs(X) .* ...
-          log(abs(X)./(2*d) + sqrt(1 + (abs(X)./(2*d)).^2)) - ...
-          sqrt(4*d*d + abs(X).^2)));
+  abs_X = abs(X);
+  sqrt_part = sqrt(1 + (abs_X./(2*d)).^2);
+
+  f = sum(sum(abs_X .* ...
+          log(abs_X./(2*d) + sqrt_part) - ...
+          sqrt(4*d*d + abs_X.^2)));
 
   % compute the vector gradient.
-  df = X .* log(abs(X)./(2*d) + sqrt(1 + (abs(X)./(2*d)).^2)) ./ abs(X);
+  df = X .* log(abs_X./(2*d) + sqrt_part) ./ abs_X;
   return
 end
